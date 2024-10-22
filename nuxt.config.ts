3ada5aab0db5b9dadbe39c254635bb7a4cfd6f88
Alexander Gerstberger <a.gerstberger@web.de>
2024-10-22 20:20:48 +0200

    Fix wrangler configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nitro-cloudflare-dev'],
-  auth: {
-    webAuthn: true
-  }
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    nitro: {
+        preset: "cloudflare-pages"
+    },
+    modules: ['@nuxt/ui', 'nitro-cloudflare-dev'],
+    auth: {
+        webAuthn: true
+    },
 })
