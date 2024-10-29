b1724340954fafa7be404525a3908bb5e1fe65b0
Alexander Gerstberger <a.gerstberger@web.de>
2024-10-29 10:42:39 +0100

    Make inputs in new form required

--- components/CreateTransfer.vue
+++ components/CreateTransfer.vue
@@ -71,16 +71,16 @@ const items = [
       <template v-if="item.key === 'new'">
         <UForm :schema="schema" :state="state" @submit="onSubmit">
           <USelectMenu v-model="state.receiver" :searchable="searchUsers" name="receiver"
-                       option-attribute="username" value-attribute="id" class="inline-block w-fit min-w-20"/>
+                       option-attribute="username" value-attribute="id" class="inline-block w-fit min-w-20" required/>
           owes
           <USelectMenu v-model="state.sender" :searchable="searchUsers" name="sender"
-                       option-attribute="username" value-attribute="id" class="inline-block w-fit min-w-20"/>
-          <UInput v-model="state.amount" type="number" class="inline-block w-20 ml-1" placeholder="Amount"/>
+                       option-attribute="username" value-attribute="id" class="inline-block w-fit min-w-20" required/>
+          <UInput v-model="state.amount" type="number" class="inline-block w-20 ml-1" placeholder="Amount" required/>
           €
           for
-          <UInput v-model="state.description" type="text" class="inline-block w-52"/>
+          <UInput v-model="state.description" type="text" class="inline-block w-52" required/>
           on
-          <UInput v-model="state.date" type="date" class="inline-block"/>
+          <UInput v-model="state.date" type="date" class="inline-block" required/>
           <UButton type="submit">Submit</UButton>
         </UForm>
       </template>
