dd631d27206f775d1e7956a62a86937016a3ff0e
Alexander Gerstberger <a.gerstberger@web.de>
2025-01-20 10:18:09 +0100

    Add profile icon with link to /settings to top bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-
+const items = [
+  [
+    {
+      label: "Settings",
+      to: "/settings",
+      icon: "i-material-symbols:settings"
+    }
+  ]
+]
+const {loggedIn, user} = getUserSession();
 </script>
 
 <template>
   <UContainer>
     <div class="flex flex-row justify-between py-3">
       <NuxtLink to="/" class="text-3xl">Debts</NuxtLink>
-      <NuxtLink to="/new-transfer" class="text-4xl">+</NuxtLink>
+      <div class="flex flex-row items-center">
+        <NuxtLink to="/new-transfer" class="text-4xl gap-1">+</NuxtLink>
+        <UDropdown :items="items" v-if="loggedIn">
+          <UAvatar :alt="user?.name"/>
+        </UDropdown>
+      </div>
     </div>
     <slot/>
   </UContainer>
