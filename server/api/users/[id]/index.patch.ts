a51f1af14f840e71895ab776d061241716f04618
Alexander Gerstberger <a.gerstberger@web.de>
2025-02-27 22:24:47 +0100

    Add missing authorization check in index.patch.ts

--- server/api/users/[id]/index.patch.ts
+++ server/api/users/[id]/index.patch.ts
@@ -1,8 +1,10 @@
 import {createUpdateSchema} from "drizzle-zod";
 import {tables, useDrizzle} from "~/db/db";
 import {eq} from "drizzle-orm";
+import {parseUserSession} from "~/utils/parseUserSession";
 
 const patchUserSchema = createUpdateSchema(tables.users);
+const unauthorized = createError({statusCode: 401, message: "Unauthorized"})
 
 export default defineEventHandler(async (event) => {
     const result = await readValidatedBody(event, patchUserSchema.safeParse);
@@ -12,6 +14,22 @@ export default defineEventHandler(async (event) => {
     }
 
     const id = getRouterParam(event, "id")!;
+
+    const token = getCookie(event, "jwt");
+
+    if (!token) {
+        throw unauthorized
+    }
+
+    const userSession = await parseUserSession(token, useRuntimeConfig(event));
+
+    if (!userSession.loggedIn) {
+        throw unauthorized
+    }
+
+    if (userSession.user.id !== parseInt(id)) {
+        throw createError({statusCode: 403, message: "You are not allowed to update this user."})
+    }
 
     const db = useDrizzle(event.context.cloudflare.env.DB);
 
