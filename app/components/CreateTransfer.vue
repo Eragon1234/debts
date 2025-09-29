308ed10b3843b2588dc66bf442d026bb52aa3a0a
Alexander Gerstberger <alexander@gerstberger.me>
2025-09-29 21:41:19 +0200

    Fix that the CreateTransfer form user USelectMenus still used the removed :searchable prop

--- app/components/CreateTransfer.vue
+++ app/components/CreateTransfer.vue
@@ -38,27 +38,50 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   })
 }
 
-async function searchUsers(query: string) {
-  return $fetch("/api/users/search", {
-    query: {
-      query: query,
-    }
-  })
-}
+const receiversSearchTerm = ref<string>('');
+const { data: receiversSearchItems, status: receiversSearchStatus } = await useFetch('/api/users/search', {
+  query: {
+    query: receiversSearchTerm.value,
+  }
+})
+
+const senderSearchTerm = ref<string>('');
+const { data: senderSearchItems, status: senderSearchStatus } = await useFetch('/api/users/search', {
+  query: {
+    query: receiversSearchTerm.value,
+  }
+})
 </script>
 
 <template>
   <UForm :schema="createTransferSchema" :state="state" @submit="onSubmit">
-    <USelectMenu v-model="state.receivers" :searchable="searchUsers" name="receiver"
-                 option-attribute="username" class="inline-block w-fit min-w-28" required
-                 multiple>
+    <USelectMenu
+        v-model="state.receivers"
+        name="receiver"
+        :items="receiversSearchItems"
+        v-model:search-term="receiversSearchTerm"
+        :loading="receiversSearchStatus === 'pending'"
+        option-attribute="username"
+        class="inline-block w-fit min-w-28"
+        required
+        multiple
+    >
       <template #label>
         <span v-if="state.receivers?.length" class="truncate">{{ state.receivers.map(r => r.username).join(', ') }}</span>
       </template>
     </USelectMenu>
     owes
-    <USelectMenu v-model="state.sender" :searchable="searchUsers" name="sender"
-                 option-attribute="username" value-attribute="id" class="inline-block w-fit min-w-28" required/>
+    <USelectMenu
+        v-model="state.sender"
+        name="sender"
+        :items="senderSearchItems"
+        v-model:search-term="senderSearchTerm"
+        :loading="senderSearchStatus === 'pending'"
+        option-attribute="username"
+        value-attribute="id"
+        class="inline-block w-fit min-w-28"
+        required
+    />
     <UInput v-model="state.amount" type="number" class="inline-block w-28 ml-1" placeholder="Amount" required/>
     €
     for
