10875bc4dd3bda7fe6d36373a2db507e08b3888e
Alexander Gerstberger <alexander@gerstberger.me>
2025-09-29 21:48:49 +0200

    Fix that createTransferSchema was only imported as a type

--- app/components/CreateTransfer.vue
+++ app/components/CreateTransfer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {z} from "zod";
 import type {FormSubmitEvent} from "#ui/types";
-import type {createTransferSchema} from "#shared/schemas/CreateTransferSchema";
+import {createTransferSchema} from "#shared/schemas/CreateTransferSchema";
 
 definePageMeta({
   middleware: "auth"
