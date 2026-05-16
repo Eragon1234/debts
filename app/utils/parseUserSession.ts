c53f4c9efca88a290ef6d16a4934f750f0972bea
Alexander Gerstberger <alexander@gerstberger.me>
2026-05-16 23:32:12 +0200

    Add getUserSession helper to make userSession parsing less repetitive

--- app/utils/parseUserSession.ts
+++ app/utils/parseUserSession.ts
@@ -1,6 +1,13 @@
 import type {UserSession} from "~/composables/session";
 import {importSPKI, jwtVerify} from "jose";
 import type {RuntimeConfig} from "nuxt/schema";
+import type {H3Event} from "h3";
+
+export async function getUserSession(event: H3Event): Promise<UserSession> {
+   const runtimeConfig = useRuntimeConfig(event);
+   const token = getCookie(event, "jwt") ?? null;
+   return await parseUserSession(token, runtimeConfig);
+}
 
 export async function parseUserSession(token: string | null, runtimeConfig: RuntimeConfig): Promise<UserSession> {
     if (!token) {
