4b380b182675dfdf84dfdaadbfa02e68e9dfe4c6
Alexander Gerstberger <alexander@gerstberger.me>
2026-07-20 02:00:42 +0200

    Fix that the OIDC connection card title did not interpolate correctly and further improve styling

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -12,14 +12,17 @@ const {data: oidcStatus} = await useFetch("/api/oidc/status");
 </script>
 
 <template>
-  <UpdateUserInfoForm/>
+  <div class="flex flex-col gap-4">
+    <UpdateUserInfoForm/>
 
-  <ChangePasswordForm/>
+    <ChangePasswordForm/>
 
-  <UPageCard v-if="runtimeConfig.public.oidcDiscoveryURL"
-             title="Connect to {{ runtimeConfig.public.oidcName ?? 'OIDC' }}">
-    <p v-if="oidcStatus?.connected" class="text-lg">Already connected!</p>
-    <UButton v-else to="/api/oidc/login" external class="text-lg">Connect</UButton>
-  </UPageCard>
+    <UPageCard v-if="runtimeConfig.public.oidcDiscoveryURL"
+               :title="`Connect to ${ runtimeConfig.public.oidcName ?? 'OIDC' }`"
+               variant="subtle">
+      <p v-if="oidcStatus?.connected" class="text-lg">Already connected!</p>
+      <UButton v-else to="/api/oidc/login" external class="text-lg">Connect</UButton>
+    </UPageCard>
+  </div>
 </template>
 
